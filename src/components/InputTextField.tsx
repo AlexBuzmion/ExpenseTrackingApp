b8a903e51bbca76963eaf7bb55eba4da15cad2e3
Alex Buzmion <alex.buzmionii@gmail.com>
2025-04-03 17:12:42 -0700

    readded flex:1 on the input text field to separate the security/eye icon.

--- src/components/InputTextField.tsx
+++ src/components/InputTextField.tsx
@@ -70,7 +70,7 @@ const styles = StyleSheet.create({
         borderColor: '#ccc',
     },
     input: {
-        // flex: 1,
+        flex: 1,
         // padding: 10,
         height: Platform.OS === 'ios' ? 56 : 46,
         fontSize: 16,
